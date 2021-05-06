9e9d9ab2243f1ce97440a4c33b1a1a0acdb3e00c
mynameiskatchy <karchietrinh@gmail.com>
2021-05-06 00:22:30 -0400

    Create DOM element for task list (4:40)

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 const listsContainer = document.querySelector('[data-lists]')
 
-let lists = []
+let lists = [
+    'name',
+    'todo'
+]
+
+function render() {
+    clearElement(listsContainer)
+    
+    lists.forEach(list => {
+        // create DOM element accordingly
+        const listElement = document.createElement('li')
+        listElement.classList.add("list-name")
+        listElement.innterText = list
+    })
+}
+
+
+function clearElement(element) {
+// To clear out given element
+}
